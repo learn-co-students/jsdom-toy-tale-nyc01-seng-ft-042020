5d86048e0f4a9acb824d1a92df187f19b99d76a9
Jessica <github email address>
2020-06-04 16:05:17 -0400

    second push

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 let addToy = false;
 
-const toyCollection = document.querySelector("toy-collection")
-console.log(toyCollection)
+const toyCollection = document.querySelector("#toy-collection")
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -19,9 +18,28 @@ document.addEventListener("DOMContentLoaded", () => {
   let data = fetch('http://localhost:3000/toys')
         .then( res => res.json() )
         .then( json => information(json) )
-  function information(toyInfo){
-    const toyList = document.createElement('div')
-    toyCollection.appendChild(toyList)
+
+  function createToy(toyObj){
+    const toyDiv = document.createElement('div')
+    toyDiv.className = 'card'
+      
+    toyDiv.innerHTML = `
+    <h2>${toyObj.name}</h2>
+    <img src="${toyObj.imageUrl}
+    <p> 0 Likes </p>
+    <button class="like-btn">Like <3</button>
+    `
+    console.log(toyDiv)
+    return toyDiv
   }
 
+  function information(toyInfo){
+    toyInfo.forEach(function(toyObj){
+      createToy(toyObj)
+    })
+    // toyCollection.appendChild(toyDiv)
+  }
+
+  
 });
+
