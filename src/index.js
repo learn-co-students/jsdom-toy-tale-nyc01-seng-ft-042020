1176f220150d4991ec5bfbddb968ea04ca61297b
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-06-04 16:37:13 -0400

    4:37

--- src/index.js
+++ src/index.js
@@ -1,60 +1,77 @@
+/* eslint-disable no-use-before-define */
 let addToy = false;
 
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
+document.addEventListener('DOMContentLoaded', () => {
+  const addBtn = document.querySelector('#new-toy-btn');
+  const toyFormContainer = document.querySelector('.container');
+  addBtn.addEventListener('click', () => {
     // hide & seek with the form
     addToy = !addToy;
     if (addToy) {
-      toyFormContainer.style.display = "block";
+      toyFormContainer.style.display = 'block';
     } else {
-      toyFormContainer.style.display = "none";
+      toyFormContainer.style.display = 'none';
     }
   });
 
-  const toyCollection = document.getElementById('toy-collection')
-  const addNewToy = document.querySelector('.submit')
-  const toyForm = document.querySelector('form')
+  const toyCollection = document.getElementById('toy-collection');
+  const addNewToy = document.querySelector('.submit');
+  const toyForm = document.querySelector('form');
 
+  toyCollection.addEventListener('click', e => {
+    const name = e.target.parentNode.children[0].textContent;
+    const likesString = e.target.parentNode.children[3].textContent;
+    const count = parseInt(likesString.split(' ')[0]);
+    e.target.parentNode.children[3].textContent = `${count + 1} Likes `;
 
-  addNewToy.addEventListener("click", function(e) {
+    fetch(
+      `http://localhost:3000/toys/${e.target.parentNode.children[1].textContent}`,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({
+          likes: count + 1,
+        }),
+      }
+    );
+  });
+
+  addNewToy.addEventListener('click', function(e) {
     const saveToy = {
       name: toyForm.name.value,
       image: toyForm.image.value,
-      likes: 0
-    }
-    createToy(saveToy)
-    fetch('http://localhost:3000/toys', saveToy)
-    
-    e.preventDefault()
-  })
+      likes: 0,
+    };
+    createToy(saveToy);
+    fetch('http://localhost:3000/toys', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(saveToy),
+    });
 
-
-  fetch('http://localhost:3000/toys')
-  .then(resp => resp.json())
-  .then(json => {
-    json.forEach(item => createToy(item))
+    e.preventDefault();
   });
 
+  fetch('http://localhost:3000/toys')
+    .then(resp => resp.json())
+    .then(json => {
+      json.forEach(item => createToy(item));
+    });
+
   function createToy(item) {
-    const newDiv = document.createElement('div')
-    newDiv.className = 'card'
-    newDiv.innerHTML = 
-      `<h2>${item['name']}</h2>
-      <img src=${item['image']} class="toy-avatar" />
-      <p>${item['likes']} Likes </p>
-      <button class="like-btn">Like <3</button>`
-      toyCollection.append(newDiv)
+    const newDiv = document.createElement('div');
+    newDiv.className = 'card';
+    newDiv.innerHTML = `<h2>${item.name}</h2>
+      <p hidden>${item.id}</p>
+      <img src=${item.image} class="toy-avatar" />
+      <p>${item.likes} Likes </p>
+      <button class="like-btn">Like <3</button>`;
+    toyCollection.append(newDiv);
   }
-
-  
-  
-  
-
-  
-
-
-
 });
-
