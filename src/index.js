f10d5ff7105dfe02b777904273acca8e2a5120b2
Jessica <github email address>
2020-06-04 16:21:03 -0400

    toy info complete

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 let addToy = false;
 
-const toyCollection = document.querySelector("#toy-collection")
-
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  const toyCollection = document.querySelector("#toy-collection");
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -18,28 +18,24 @@ document.addEventListener("DOMContentLoaded", () => {
   let data = fetch('http://localhost:3000/toys')
         .then( res => res.json() )
         .then( json => information(json) )
-
-  function createToy(toyObj){
-    const toyDiv = document.createElement('div')
-    toyDiv.className = 'card'
-      
-    toyDiv.innerHTML = `
-    <h2>${toyObj.name}</h2>
-    <img src="${toyObj.imageUrl}
-    <p> 0 Likes </p>
-    <button class="like-btn">Like <3</button>
-    `
-    console.log(toyDiv)
-    return toyDiv
-  }
-
+  
   function information(toyInfo){
     toyInfo.forEach(function(toyObj){
       createToy(toyObj)
     })
-    // toyCollection.appendChild(toyDiv)
   }
 
-  
+  function createToy(toyObj){
+    const toyDiv = document.createElement('div')
+    toyDiv.className = 'card'
+    toyDiv.innerHTML = `
+    <h2>${toyObj.name}</h2>
+    <img src="${toyObj.image}" height="142" width="142">
+    <p> 0 Likes </p>
+    <button class="like-btn">Like <3</button>
+    `
+    toyCollection.appendChild(toyDiv)
+  }
+
 });
 
