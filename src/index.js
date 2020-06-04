2f6ef3a2abac26bd68687253d87c793ea81118bb
carolineglass <carolineglass89@gmail.com>
2020-06-04 15:55:35 -0400

    first half

--- src/index.js
+++ src/index.js
@@ -12,4 +12,49 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  const toyCollection = document.getElementById('toy-collection')
+  const addNewToy = document.querySelector('.submit')
+  const toyForm = document.querySelector('form')
+
+
+  addNewToy.addEventListener("click", function(e) {
+    const saveToy = {
+      name: toyForm.name.value,
+      image: toyForm.image.value,
+      likes: 0
+    }
+    createToy(saveToy)
+    fetch('http://localhost:3000/toys', saveToy)
+    
+    e.preventDefault()
+  })
+
+
+  fetch('http://localhost:3000/toys')
+  .then(resp => resp.json())
+  .then(json => {
+    json.forEach(item => createToy(item))
+  });
+
+  function createToy(item) {
+    const newDiv = document.createElement('div')
+    newDiv.className = 'card'
+    newDiv.innerHTML = 
+      `<h2>${item['name']}</h2>
+      <img src=${item['image']} class="toy-avatar" />
+      <p>${item['likes']} Likes </p>
+      <button class="like-btn">Like <3</button>`
+      toyCollection.append(newDiv)
+  }
+
+  
+  
+  
+
+  
+
+
+
 });
+
