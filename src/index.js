b4eeb4597dbb36722b02b0cab9d1368698dfbe4d
Jessica <github email address>
2020-06-04 15:42:56 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 let addToy = false;
 
+const toyCollection = document.querySelector("toy-collection")
+console.log(toyCollection)
+
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
@@ -12,4 +15,13 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  let data = fetch('http://localhost:3000/toys')
+        .then( res => res.json() )
+        .then( json => information(json) )
+  function information(toyInfo){
+    const toyList = document.createElement('div')
+    toyCollection.appendChild(toyList)
+  }
+
 });
